b34c3c510b48a6afa2b4769eedc3f0c2c536fe6e
Sam L <samlitowitz@users.noreply.github.com>
2024-02-10 13:04:16 -0500

    Add missing functions for units and remove old files

---
+++ internal/gen-go-crud/gen_go_crud_test.go
@@ -0,0 +1,83 @@
+package gen_go_crud
+
+import (
+	"fmt"
+
+	"github.com/samlitowitz/protoc-gen-crud/internal/descriptor"
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+func allStringCombinations(set []string) (subsets [][]string) {
+	length := uint(len(set))
+
+	// Go through all possible combinations of objects
+	// from 1 (only first object in subset) to 2^length (all objects in subset)
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		var subset []string
+
+		for object := uint(0); object < length; object++ {
+			// checks if object is contained in subset
+			// by checking if bit 'object' is set in subsetBits
+			if (subsetBits>>object)&1 == 1 {
+				// add object to subset
+				subset = append(subset, set[object])
+			}
+		}
+		// add subset to subsets
+		subsets = append(subsets, subset)
+	}
+	return subsets
+}
+
+func goTypeByFieldDescType(defs []*descriptor.Field) (string, error) {
+	if len(defs) < 1 {
+		return "", fmt.Errorf("no fields found, cannot build key type")
+	}
+	if len(defs) == 1 {
+		def := defs[0]
+		switch *def.Type {
+		case descriptorpb.FieldDescriptorProto_TYPE_DOUBLE:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_FLOAT:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_BOOL:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_ENUM:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_GROUP:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_MESSAGE:
+			return "", &descriptor.UnsupportedTypeError{TypName: *def.TypeName}
+
+		case descriptorpb.FieldDescriptorProto_TYPE_UINT32:
+			return "uint32", nil
+		case descriptorpb.FieldDescriptorProto_TYPE_UINT64:
+			return "uint64", nil
+
+		case descriptorpb.FieldDescriptorProto_TYPE_INT32:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_FIXED32:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_SFIXED32:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_SINT32:
+			return "int32", nil
+
+		case descriptorpb.FieldDescriptorProto_TYPE_INT64:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_FIXED64:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_SFIXED64:
+			fallthrough
+		case descriptorpb.FieldDescriptorProto_TYPE_SINT64:
+			return "int64", nil
+
+		case descriptorpb.FieldDescriptorProto_TYPE_BYTES:
+			return "string", nil
+
+		case descriptorpb.FieldDescriptorProto_TYPE_STRING:
+			return "string", nil
+		}
+	}
+	return "string", nil
+}
