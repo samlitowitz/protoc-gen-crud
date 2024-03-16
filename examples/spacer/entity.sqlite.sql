fd8fdf3b2f56ed34898a03a3e8ac3fa28b05b052
Sam L <samlitowitz@users.noreply.github.com>
2024-03-16 08:12:32 -0400

    Add spacer example, but it's missing many to many support

---
+++ examples/spacer/entity.sqlite.sql
@@ -0,0 +1,8 @@
+
+DROP TABLE IF EXISTS "entity";
+CREATE TABLE IF NOT EXISTS "entity" (
+    "id" TEXT,
+    "tags_id" TEXT,
+
+    PRIMARY KEY ("id")
+);
---
+++ examples/spacer/tag.sqlite.sql
@@ -0,0 +1,8 @@
+
+DROP TABLE IF EXISTS "tag";
+CREATE TABLE IF NOT EXISTS "tag" (
+    "id" TEXT,
+    "name" TEXT,
+
+    PRIMARY KEY ("id")
+);
